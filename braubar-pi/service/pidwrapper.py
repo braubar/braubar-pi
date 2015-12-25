62854f3584330b7129fa9f5f5c6460a4eb409c22
Oliver Fesseler <ofesseler@gmail.com>
2015-12-25 20:13:34 +0100

    adds pid libs and beginning of a wwrapper for it.

--- braubar-pi/service/pidwrapper.py
+++ braubar-pi/service/pidwrapper.py
@@ -1,22 +1,40 @@
-from PID import PID
+#from PID import PID as PID
+from PIDs import Pid as PID
 from threading import Thread, Timer
 
 
-class PIDWrapper(PID):
+class PIDWrapper():
     pid = None
     thread = None
 
-    def __init__(self):
-        self.thread = Thread(target=self.do_work, name="BrewPID", args=())
+    def __init__(self, temp=45.0, below=-0.5, above=0.5):
+        self.temp = temp
+        self.thread = Thread(target=self._do_work, name="BrewPID", args=())
 
-    def update(self, feedback_value):
-        self.pid.update(feedback_value)
-
+    def update(self, set):
+        self.pid.set(set)
     def start(self):
         self.thread.run()
 
         # assert 0
 
-    def do_work(self):
-        self.pid = PID(0.2, 0.0, 0.0)
+    def get(self):
+        self.pid.get()
+
+    def _do_work(self):
+        self.pid = PID(P=0.8, I=0.1, D=0.1, point=45.0, below=-0.5, above=0.5)
+        self.pid.set(10)
+        # self.pid.
         print("PID started")
+
+
+def pidtest():
+    p = PIDWrapper()
+    p.start()
+    for i in range(10):
+        p.update(i)
+
+        print(p.get())
+
+
+pidtest()
