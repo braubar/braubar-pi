697b7f81954bae403aaaa807f62bd4e022e4ff3b
Oliver Fesseler <ofesseler@gmail.com>
2016-01-04 00:53:42 +0100

    added markdown support and changed theme to rtd

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 #
 # BrauBar documentation build configuration file, created by
 # sphinx-quickstart on Mon Jan  4 00:15:37 2016.
@@ -40,10 +40,17 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# parser for markdown
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = '.rst', '.md'
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
@@ -114,7 +121,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
