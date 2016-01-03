c71cf8c19f794315e78c91f93a83e8e95aedc053
Oliver Fesseler <ofesseler@gmail.com>
2016-01-03 01:27:34 +0100

    added function to create files if they don't exist.
    the temp file for exchanging the temperature values and for the next
    state have to be existent for the proper function of braubar-pi so they
    will be created if they don't exist.

--- braubar-pi/brewdaemon.py
+++ braubar-pi/brewdaemon.py
@@ -138,6 +138,11 @@ class BrewDaemon:
         self.powerstrip.all_off()
         self.powerstrip.logout()
 
+    def assureComFileExists(self):
+        f = open("next_state.brew", 'w')
+        f.close()
+        f = open("/tmp/braubar.temp", 'w')
+        f.close()
 
 if __name__ == "__main__":
     brew_daemon = BrewDaemon()
